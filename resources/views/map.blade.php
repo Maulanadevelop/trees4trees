bee03dff716a4d523d7e1069ba2b2254d1c25e34
rizqipetcentral <rizqi@petcentral.id>
2024-10-08 22:08:56 +0700

    showing dots on maps and popup on hover

--- resources/views/map.blade.php
+++ resources/views/map.blade.php
@@ -1,26 +1,108 @@
 <!DOCTYPE html>
 <html>
+
 <head>
-<meta charset="utf-8">
-<title>Display a map on a webpage</title>
-<meta name="viewport" content="initial-scale=1,maximum-scale=1,user-scalable=no">
-<link href="https://api.mapbox.com/mapbox-gl-js/v3.7.0/mapbox-gl.css" rel="stylesheet">
-<script src="https://api.mapbox.com/mapbox-gl-js/v3.7.0/mapbox-gl.js"></script>
-<style>
-body { margin: 0; padding: 0; }
-#map { position: absolute; top: 0; bottom: 0; width: 100%; }
-</style>
+    <meta charset="utf-8">
+    <title>Display a map on a webpage</title>
+    <meta name="viewport" content="initial-scale=1,maximum-scale=1,user-scalable=no">
+    <link href="https://api.mapbox.com/mapbox-gl-js/v3.7.0/mapbox-gl.css" rel="stylesheet">
+    <script src="https://api.mapbox.com/mapbox-gl-js/v3.7.0/mapbox-gl.js"></script>
+    <script src="https://code.jquery.com/jquery-3.7.1.slim.js"
+        integrity="sha256-UgvvN8vBkgO0luPSUl2s8TIlOSYRoGFAX4jlCIm9Adc=" crossorigin="anonymous"></script>
+    <style>
+        body {
+            margin: 0;
+            padding: 0;
+        }
+
+        #map {
+            position: absolute;
+            top: 0;
+            bottom: 0;
+            width: 100%;
+        }
+
+        .marker {
+            background-color: white;
+            border: 3px solid lightseagreen;
+            padding: 6px;
+            border-radius: 100px;
+            position: absolute;
+        }
+
+        .marker:hover {
+            background-color: white;
+            border: 3px solid lightseagreen;
+            padding: 12px;
+            border-radius: 100px;
+            position: absolute;
+            cursor: pointer;
+        }
+
+        .popup {
+            position: absolute;
+            padding: 15px 25px;
+            margin-left: 19px;
+            margin-top: 8px;
+            font-size: 16px;
+            background-color: white;
+            box-shadow: 1px 1px 5px dimgrey;
+            border-radius: 2px 15px 15px 15px;
+            color: #36454f;
+        }
+    </style>
 </head>
+
 <body>
-<div id="map"></div>
+    <div id="map"></div>
+    @foreach($lands as $key => $land)
+        <div id="marker-{{$key}}" class="marker"></div>
+    @endforeach
+</body>
+
 <script>
-	mapboxgl.accessToken = 'pk.eyJ1IjoibWF1bGFuYWRldmVsb3AiLCJhIjoiY20yMGJwZXhuMGY1NTJrcHA4MW83YThuOSJ9.2zcYyZ4YS9mzFMhSuR1aIQ';
+    const default_coord = {
+        latitude: -6.916399183735122,
+        longitude: 107.61929573865112
+    }
+    const lands = @json($lands)
+
+    mapboxgl.accessToken = '{{env('MAPBOX_TOKEN')}}';
     const map = new mapboxgl.Map({
         container: 'map', // container ID
-        center: [110.42426254790294, -7.026351176160473], // starting position [lng, lat]. Note that lat must be set between -90 and 90
-        zoom: 14 // starting zoom
+        center: [
+            lands[0]?.longitude ?? default_coord.longitude,
+            lands[0]?.latitude ?? default_coord.latitude
+        ], // starting position [lng, lat]. Note that lat must be set between -90 and 90
+        zoom: 15 // starting zoom
     });
+
+    $(document).ready(() => {
+        map.on('load', () => {
+            lands.map((land, key) => {
+                let marker = new mapboxgl.Marker(document.getElementById(`marker-${key}`)).setLngLat([
+                    land?.longitude ?? default_coord.longitude,
+                    land?.latitude ?? default_coord.latitude
+                ]).addTo(map);
+
+                $(`#marker-${key}`).on('mouseenter', () => {
+                    $(`#marker-${key}`).append(`
+                    <div class="popup">
+                    <p>No. Lahan : ${land.land_no}</p>
+                    <p>No. Petani : ${land.farmer_no}</p>
+                    </div>
+                    `);
+                });
+                $(`#marker-${key}`).on('mouseleave', () => {
+                    $(`#marker-${key}`).empty();
+                });
+
+                $(`#marker-${key}`).on('doubleclick', () => {
+                    console.log('wkwk')
+                });
+            })
+        })
+    })
 </script>
 
-</body>
 </html>
